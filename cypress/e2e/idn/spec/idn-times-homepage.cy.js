d293dda9d6cbdd51dadde12a30e1390c6b155505
testtsts <ihsannur0610@gmail.com>
2025-05-21 13:09:25 +0700

    move ignore uncaught exception

--- cypress/e2e/idn/spec/idn-times-homepage.cy.js
+++ cypress/e2e/idn/spec/idn-times-homepage.cy.js
@@ -1,6 +1,12 @@
 // import { HomePage } from '../pages/HomePage';
 const {HomePage,CONNECT_SUBDOMAIN, PROFILE_SUBDOMAIN, MEDIA_SUBDOMAIN} = require('../pom/homepage')
 
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from failing the test
+    return false;
+  });
+
 describe('HomePage Navigation', () => {
   beforeEach(() => {
     HomePage.visit();
