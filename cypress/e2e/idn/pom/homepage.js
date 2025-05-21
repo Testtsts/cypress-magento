3e2ced8ceeddf595359094df5d187c1ab5a82e09
testtsts <ihsannur0610@gmail.com>
2025-05-21 09:30:06 +0700

    idn testcase

---
+++ cypress/e2e/idn/pom/homepage.js
@@ -0,0 +1,77 @@
+// cypress/pages/Homepage.js
+
+exports.HomePage = class Homepage {
+  static visit() {
+
+    cy.intercept('GET', 'https://*googlesyndication.com*', { statusCode: 200, body: {} }).as('mockedRequest');
+
+    cy.visit('https://www.idntimes.com/',{
+      waitForAnimations: false,
+
+      // onBeforeLoad: (win) => {
+      //   // cy.stub(win,)
+      //   return new Cypress.Promise((resolve) => {
+      //     const loadEvent = new Event('load');
+      //     win.document.addEventListener('DOMContentLoaded', () => {
+      //       console.log('DOMContentLoaded event has been fired');
+      //       win.dispatchEvent(loadEvent);
+      //     });
+      //   });
+      // },
+    });
+    cy.window().then((win) => {
+      // Here, you might directly call 'win.dispatchEvent' as you’ve done before or check some condition
+      if (win.document.readyState === 'complete') {
+        // Your logic if the page is ready
+      } else {
+        // Force the 'load' event if needed
+        const loadEvent = new Event('load');
+        win.dispatchEvent(loadEvent);
+      }
+    });
+
+    
+    cy.get('[data-testid="header-logo-img"]'  ).should('be.visible');
+  }
+
+  static clickGenZMemilih() {
+    return cy.contains('#GenZMemilih').click();
+  }
+
+  static clickJoinButton() {
+    return cy.get('[data-testid="join-btn"]').click();
+  }
+
+  static clickProfilDokter() {
+    return cy.contains('Profil Dokter')
+    .invoke('removeAttr','target')
+    .click({ animationDistanceThreshold: 10 });
+  }
+
+  static clickCommunityMenu() {
+    return cy.get('[data-testid="menu-community-link"]').click();
+  }
+
+  static clickBusinessMenu() {
+    return cy.get('[data-testid="menu-business-link"]').click();
+  }
+
+  static clickTechMenu() {
+    return cy.get('[data-testid="menu-tech-link"]').click();
+  }
+
+  static clickHealthMenu() {
+    return cy.get('[data-testid="menu-health"]').click();
+  }
+
+  static clickCareerSidebar() {
+    return cy.get('[data-testid="sidebar-career"] > a')
+    .invoke('removeAttr','target')
+    .click();
+  }
+
+};
+
+exports.PROFILE_SUBDOMAIN = 'https://profil.idntimes.com'
+exports.CONNECT_SUBDOMAIN = 'https://connect.idn.media'
+exports.MEDIA_SUBDOMAIN = 'https://www.idn.media'
---
+++ cypress/e2e/idn/spec/idn-times-homepage.cy.js
@@ -0,0 +1,48 @@
+// import { HomePage } from '../pages/HomePage';
+const {HomePage,CONNECT_SUBDOMAIN, PROFILE_SUBDOMAIN, MEDIA_SUBDOMAIN} = require('../pom/homepage')
+
+describe('HomePage Navigation', () => {
+  beforeEach(() => {
+    HomePage.visit();
+  });
+
+  it.skip('should redirect user to new page when clicking #GenZMemilih', () => {
+    HomePage.clickGenZMemilih();
+    cy.url().should('include', 'genzmemilih.idntimes.com');
+  });
+
+  it.skip('should redirect to login page when clicking Daftar/Masuk', () => {
+    HomePage.clickJoinButton();
+    cy.url().should('include', 'connect.idn.media');
+  });
+
+  it('should redirect to doctor profile when clicking Profil Dokter', () => {
+    HomePage.clickProfilDokter();
+    cy.origin(PROFILE_SUBDOMAIN,()=>{
+        cy.url().should('include', 'profil.idntimes.com');
+    });
+  });
+
+  it('should redirect to login page when clicking Community Menu', () => {
+    HomePage.clickCommunityMenu();
+    cy.origin(CONNECT_SUBDOMAIN,()=>{
+        cy.url().should('include', 'connect.idn.media');
+    })
+  });
+
+  it('should redirect to selected news page when clicking headline news', () => {
+    HomePage.clickTechMenu();
+    cy.url().should('include', '/tech');
+    HomePage.clickBusinessMenu();
+    cy.url().should('include', '/business');
+    HomePage.clickHealthMenu();
+    cy.url().should('include', '/health');
+  });
+
+  it('should redirect to career page when clicking career sidebar', () => {
+    HomePage.clickCareerSidebar();
+    cy.origin(MEDIA_SUBDOMAIN, ()=>{
+        cy.url().should('include', 'idn.media/career');
+    });
+  });
+});
